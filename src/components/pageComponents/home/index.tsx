3bb39062c8d3ee5b3a139ae22e36130a1cb952d8
Angelo Dias <oiangelodias@gmail.com>
2021-01-19 20:58:18 -0300

    Add fake projects for example

--- src/components/pageComponents/home/index.tsx
+++ src/components/pageComponents/home/index.tsx
@@ -62,6 +62,78 @@ export default function Home() {
             to="buttonpath"
             alt="Nome do projeto"
           />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
+          <Card
+            square
+            image="https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            to="buttonpath"
+            alt="Nome do projeto"
+          />
         </Mosaic>
       </Section>
       <Section title="Como funciona" bg="clear">
