2cf251b5107e6a1d70b621aac9aa3aa2e4a08217
Angelo Dias <oiangelodias@gmail.com>
2021-01-26 08:46:57 -0300

    Add auto close on hamburguer menu

--- src/components/atoms/TopBar/Hamburger/index.tsx
+++ src/components/atoms/TopBar/Hamburger/index.tsx
@@ -1,12 +1,28 @@
+import { nanoid } from 'nanoid'
+import { cloneElement, ReactElement, useState } from 'react'
 import { Menu, MenuItems } from './style'
 
 export default function Hamburger({ children }) {
+  const [opened, setOpened] = useState(false)
+
   return (
     <Menu>
       <label htmlFor="hamburger">☰</label>
-      <input type="checkbox" id="hamburger" />
+      <input
+        type="checkbox"
+        id="hamburger"
+        checked={opened}
+        onChange={() => setOpened(!opened)}
+      />
 
-      <MenuItems>{children}</MenuItems>
+      <MenuItems>
+        {children.map((each: ReactElement) => {
+          return cloneElement(each, {
+            key: nanoid(),
+            onClick: () => setOpened(false),
+          })
+        })}
+      </MenuItems>
     </Menu>
   )
 }
